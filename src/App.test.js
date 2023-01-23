e1f442385d78e0476826e8adf584b1d1e43512ab
axl1dev <axelpalacioos@gmail.com>
2023-01-23 04:31:54 -0600

    fix: solve to import axios in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Show render text title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Estadistícas del programa de lealtad/i);
   expect(linkElement).toBeInTheDocument();
 });
